a3baebf44522fe350f28ed9ebaca53f4040ce700
sarahwesley <92330391+sarahwesley@users.noreply.github.com>
2022-01-03 16:49:31 -0500

    express required, port added, app initialized

---
+++ server.js
@@ -0,0 +1,16 @@
+const express = require('express');
+
+const PORT = process.env.PORT || 3001;
+const app = express();
+
+// Express middleware
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+
+app.use((req, res) => {
+    res.status(404).end();
+});
+
+app.listen(PORT, () => {
+    console.log(`server running on port ${PORT}`);
+});
